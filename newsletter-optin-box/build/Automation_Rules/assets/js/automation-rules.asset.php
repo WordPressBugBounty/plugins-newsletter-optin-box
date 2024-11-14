72d1a3ad4efa05f054ecd6e0c2e506b4d43acc40
Sean Murphy <sean@iamseanmurphy.com>
2024-11-14 16:35:15 +0000

    3.6.3

--- newsletter-optin-box/build/Automation_Rules/assets/js/automation-rules.asset.php
+++ newsletter-optin-box/build/Automation_Rules/assets/js/automation-rules.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd44e2d534e1734b7c7db');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1bc71736fdedfdf67332');
